af92d73183fbb0e06bb7fbd53a7c57d8c1f781d9
Ray Mak <projectraymak@gmail.com>
2024-04-10 12:20:58 -0700

    completed userstats

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# betstats_scraper
+# Betting Stats Scraper
+
+First commit - created a scraper to collect the matchups and usernames of users who posted their picks. There was a bit of difficulty navigating to the element that contained these matchups. After locating these matchup elements, I scraped the fighter names, users, and user betting information from each element and stored them in a variable, fighter_pairs
+
+Second commit - The first commit was actually to scrape user data from the first matchup. I did it to practice the code. This second commit is to integrate that code into my fighter pairs function so that it scrapes the user data for each matchup.
+
+Third commit - I created a separate python file to scrape individual user data. This way I can test the code without having to run it on every user. 
--- userstats.py
+++ userstats.py
@@ -13,17 +13,38 @@ step_5 = step_4.find("tr")
 step_6 = step_5.find("td")
 step_7 = step_6.find("table")
 
-betting_stats = []
+#find avg unit size
+userpanel = userpage.select_one("#frm_profile")
+userpanel_table = userpanel.find("table")
+tr2 = userpanel_table.find_all("tr")[1]
+tr2_td = tr2.find_all("td")[1]
+tr2_td_table = tr2_td.find("table")
+tr2_td_table_tr = tr2_td_table.find_all("tr")[5]
+avg_bet = tr2_td_table_tr.find_all("td")[3].text.strip()
+
+#find straight pick stats
+target_trs = step_7.find_all("tr", recursive=False)[3]
+target_trs_td = target_trs.find("td")
+target_trs_td_table = target_trs_td.find("table")
+sp_trs = target_trs_td_table.find_all("tr")
+
+big_fav_tds = sp_trs[1].find_all("td")
+sm_fav_tds = sp_trs[2].find_all("td")
+sm_dog_tds = sp_trs[3].find_all("td")
+big_dog_tds = sp_trs[4].find_all("td")
+
+big_fav = {"total_picks": big_fav_tds[1].text, "roi": big_fav_tds[7].text}
+sm_fav = {"total_picks": sm_fav_tds[1].text, "roi": sm_fav_tds[7].text}
+sm_dog = {"total_picks": sm_dog_tds[1].text, "roi": sm_dog_tds[7].text}
+big_dog = {"total_picks": big_dog_tds[1].text, "roi": big_dog_tds[7].text}
+
+betting_stats = {
+    "avg_bet": avg_bet,
+    "big_fav": big_fav,
+    "sm_fav": sm_fav,
+    "sm_dog": sm_dog,
+    "big_dog": big_dog
+    }
 
 
-
-target_trs = target_table.find_all("tr")
-big_fav_tds = target_trs[1].find_all("td")
-sm_fav_tds = target_trs[2].find_all("td")
-sm_dog_tds = target_trs[3].find_all("td")
-big_dog_tds = target_trs[4].find_all("td")
-
-big_fav = {"total_picks": big_fav_tds[1], "roi": big_fav_tds[7]}
-sm_fav = {"total_picks": sm_fav_tds[1], "roi": sm_fav_tds[7]}
-sm_dog = {"total_picks": sm_dog_tds[1], "roi": sm_dog_tds[7]}
-big_dog = {"total_picks": big_dog_tds[1], "roi": big_dog_tds[7]}
+print(betting_stats)
