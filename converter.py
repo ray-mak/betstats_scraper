58207f8f690048f07c4885d202d607a636556e86
Ray Mak <projectraymak@gmail.com>
2024-04-14 13:48:18 -0700

    Removed "nan" from user_stats

--- converter.py
+++ converter.py
@@ -3,12 +3,18 @@ import json
 with open('user_stats.json', "r") as f:
     data = json.load(f)
 
+filteredData = []
 for item in data: 
+    isNan = False
     for key, value in item['user_stats'].items():
+        if "roi" in value and value["roi"] == "nan":
+            isNan=True
         if "total_picks" in value:
             item["user_stats"][key]['total_picks'] = value['total_picks'].split()[0]
         if "roi" in value: 
             item["user_stats"][key]["roi"] = value["roi"].replace("%", "")
+    if not isNan:
+        filteredData.append(item)
 
 with open("user_stats_updated.json", "w") as f:
-    json.dump(data, f, indent=2)
+    json.dump(filteredData, f, indent=2)
